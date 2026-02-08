88d23d8090e6df97108e0e946eaec127100ac914
eric <ericricky200@gmail.com>
2026-02-08 14:57:48 +0300

    04: authentication

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from "@/features/auth/components/signup-form";
+import React from "react";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
---
+++ src/features/auth/auth-utils.ts
@@ -0,0 +1,37 @@
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const requireAuth = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  return session;
+};
+
+export const requireUnAuth = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
+
+  return session;
+};
+
+export const socialAuth = async (provider: "google" | "github") => {
+  const data = await authClient.signIn.social({
+    provider,
+    callbackURL: "/",
+  });
+
+  return data;
+};
