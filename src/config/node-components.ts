b8d1c15b6100340f4f43087c46aaff7989c196f4
eric <ericricky200@gmail.com>
2026-02-14 03:53:36 +0300

    22: google form trigger

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,13 +1,15 @@
 import { InitialNode } from "@/components/initial-node";
 import { HttpRequestNode } from "@/features/executions/components/http-request/node";
+import { GoogleFormTriggerNode } from "@/features/triggers/components/google-form-trigger/node";
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
 import { NodeType } from "@/generated/prisma/enums";
 import type { NodeTypes } from "@xyflow/react";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
-  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
   [NodeType.HTTP_REQUEST]: HttpRequestNode,
+  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
+  [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTriggerNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeTypes = keyof typeof nodeComponents;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,19 +1,21 @@
 import { getExecutor } from "@/features/executions/lib/executor-registry";
 import db from "@/lib/db";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { NonRetriableError } from "inngest";
+import { googleFormTriggerChannel } from "./channels/google-form-trigger";
 import { httpRequestChannel } from "./channels/http-request";
 import { manualTriggerChannel } from "./channels/manual-trigger";
 import { inngest } from "./client";
 import { topologicalSort } from "./utils";
 
-const google = createGoogleGenerativeAI();
-
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow" },
   {
     event: "workflows/execute.workflow",
-    channels: [httpRequestChannel(), manualTriggerChannel()],
+    channels: [
+      httpRequestChannel(),
+      manualTriggerChannel(),
+      googleFormTriggerChannel(),
+    ],
   },
   async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
